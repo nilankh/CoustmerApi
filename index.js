a086eccbd90a01eed6059f7d96b902b777ec1950
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-18 22:06:48 +0530

    completed authentication part

--- index.js
+++ index.js
@@ -22,17 +22,9 @@ app.get('/api/courses', (req, res) => {
 
 // POST requests
 app.post('/api/courses', (req, res) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    const result=  Joi.validate(req.body, schema);
-    // console.log(result);
-
-
-    // authentication 
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body); //result.error
+    if(error) {
+        res.status(400).send(error.details[0].message);
         return;
     }
     const course = {
@@ -43,6 +35,41 @@ app.post('/api/courses', (req, res) => {
     res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    // if not existinfg return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    // req.params.id return strings to parse this string into integer
+    if(!course){ //404
+        res.status(404).send('The course with given id was not found.');
+    }
+    // validate
+    
+    const { error } = validateCourse(req.body); //result.error
+    
+// if invalid, return 400 - BAd request
+    if(error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    // Update course
+    course.name = req.body.name;
+    res.send(course);
+    // Return the updated course
+
+});
+
+
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return  Joi.validate(course, schema);
+}
+
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
     // req.params.id return strings to parse this string into integer
@@ -52,6 +79,7 @@ app.get('/api/courses/:id', (req, res) => {
         res.send(course);
     }
 });
+
 
 //how to read params
 // app.get('/api/posts/:year/:month', (req, res) => {
