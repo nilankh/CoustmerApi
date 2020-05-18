b629b5273abf7c36354d5b0cfa502d2a2faca7a3
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-18 20:35:33 +0530

    now checking at postman working fine

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const courses = [
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2'},
@@ -17,6 +18,16 @@ app.get('/api/courses', (req, res) => {
 // app.get('/api/courses/:id', (req, res) => {
 //     res.send(req.params.id);
 // });
+
+// POST requests
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(courses);
+    res.send(course);
+});
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
